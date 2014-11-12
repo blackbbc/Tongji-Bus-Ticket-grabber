74badce26c3a6cedc1a4741093fb04083904c611
blackbbc <505968815@qq.com>
2014-11-12 21:14:12 +0800

    alpha 0.1

---
+++ getTicket.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+date=$(date +%H%M%S)
+
+while [ $date -lt 060500 ]
+do
+
+    sleep 0.5
+
+    curl 'http://jiading.tongji.edu.cn:8080/TJbus/getTicketServlet?username=1352388&get_ticket_time=1415772567939&bus_id=1_1'
+
+    date=$(date +%H%M%S)
+done
+
+echo $date;
+
+exit 1;
